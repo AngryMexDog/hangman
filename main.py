eb118b793be7ea031e61809c56c24b3f8a370fe2
AngryMexDog <aortiz19.depad.sj@gmail.com>
2023-09-06 09:45:39 -0600

    Checks wrong answer and shows game over screen

--- main.py
+++ main.py
@@ -8,3 +8,22 @@ lives = 6
 print(f"The solution is {chosen_word}")
 
 display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    if guess in display:
+        print("You've already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life")
+        lives -= 1
+        if lives == 0:
+            end_of_game:True
+            print("You lose.")
+
